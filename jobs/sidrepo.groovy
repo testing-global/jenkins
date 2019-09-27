ed9ea92035c16d7a22f4bbd2afe7ac0ac56d838b
Akshay Chougule <akchougule5607@gmail.com>
2019-09-27 11:22:02 +0530

    added repos

---
+++ jobs/sidrepo.groovy
@@ -0,0 +1,19 @@
+node('master') {
+
+    stage('Checkout git repo') 
+
+    stage('Seed tt-devops repository jobs') {
+        withFolderProperties {
+            jobDsl(
+                targets: 'jobs/sidrepodsl.groovy',
+                additionalParameters: [
+                    jenkinsBranch: JENKINS_BRANCH,
+                ],
+                lookupStrategy: 'JENKINS_ROOT',
+                removedJobAction: 'DELETE',
+                removedViewAction: 'DELETE',
+                sandbox: true,
+            )
+        }
+    }
+
---
+++ jobs/sidrepodsl.groovy
@@ -0,0 +1,48 @@
+def baseUrl = "https://github.com/testing-global"
+def repositories = "repositories-${JENKINS_SERVER == 'repo-prod' ? 'repo-prod' : 'repo-test'}.txt"
+repositories = readFileFromWorkspace(repositories).replaceAll('#.*', '').split()
+
+repositories.each { repository ->
+    job = pipelineJob("tt-devops/jenkins/repositories/$repository") {
+        properties {
+            authorizeProjectProperty {
+                strategy {
+                    triggeringUsersAuthorizationStrategy()
+                }
+            }
+        }
+        definition {
+            cpsScmFlowDefinition {
+                scm {
+                    gitSCM {
+                        userRemoteConfigs {
+                            userRemoteConfig {
+                                url "$baseUrl/${repository}.git"
+                                credentialsId 'github-checkout'
+                                name 'origin'
+                                refspec ''
+                            }
+                        }
+                        branches {
+                            branchSpec {
+                                name jenkinsBranch
+                            }
+                        }
+                        doGenerateSubmoduleConfigurations false
+                        browser {
+                            githubWeb {
+                                repoUrl "$baseUrl/$repository"
+                            }
+                        }
+                        gitTool ''
+                    }
+                }
+                scriptPath 'DevOps1.groovy'
+            }
+        }
+        environmentVariables {
+            env 'PHASE', 'seed'
+        }
+    }
+    queue job
+}
