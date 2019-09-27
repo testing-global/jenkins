889d3057a5e9883d707bbdd651334b104e3b451f
Akshay Chougule <akchougule5607@gmail.com>
2019-09-27 17:06:43 +0530

    changes in ttenb

--- jobs/ttenb.groovy
+++ jobs/ttenb.groovy
@@ -1,11 +1,10 @@
-    stage('Create the initial tt-devops jenkins jobs') {
-        jobDsl(
-            targets: 'jobs/ttenbdsl.groovy',
-            lookupStrategy: 'JENKINS_ROOT',
-            removedJobAction: 'DISABLE',
-            removedViewAction: 'DELETE',
-            sandbox: true,
+stage('Create the initial tt-devops jenkins jobs') {
+    jobDsl(
+        targets: 'jobs/ttenbdsl.groovy',
+        lookupStrategy: 'JENKINS_ROOT',
+        removedJobAction: 'DISABLE',
+        removedViewAction: 'DELETE',
+        sandbox: true,
         )
         
-    }
 }
