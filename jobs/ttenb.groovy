bddf91bf3a675e231b46e6bc18b7503d833f33be
Akshay Chougule <akchougule5607@gmail.com>
2019-09-30 12:53:49 +0530

    changes in enb for git checkout

--- jobs/ttenb.groovy
+++ jobs/ttenb.groovy
@@ -1,20 +1,16 @@
-def baseUrl = "https://github.com/testing-global"
-
-any () {
-
-    stage('Checkout git repo') {
-        steps {
-			checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, userRemoteConfigs: [[url: 'https://github.com/testing-global/jenkins.git']]])        	
-        }
+node('master') {
+	stage('Checkout git repo') {
+        checkout([$class: 'GitSCM', branches: [[name: '*/master']], userRemoteConfigs: [[url: 'https://github.com/testing-global/jenkins.git']]])
+    }
+    
+    stage('Create the initial tt-devops jenkins jobs') {
+        jobDsl(
+            targets: 'jobs/ttenbdsl.groovy',
+            lookupStrategy: 'JENKINS_ROOT',
+            removedJobAction: 'DELETE',
+            removedViewAction: 'DELETE',
+            sandbox: true,
+        )
     }
 
-	stage('Create the initial tt-devops jenkins jobs') {
-    	jobDsl(
-        	targets: 'jobs/ttenbdsl.groovy',
-        	lookupStrategy: 'JENKINS_ROOT',
-        	removedJobAction: 'DISABLE',
-        	removedViewAction: 'DELETE',
-        	sandbox: true,
-        )
-	}
 }
